d4d5e6d7df97964560af1562aef0513c39969b12
Inés P <inespromero@gmail.com>
2026-04-09 18:32:13 +0200

    fix seo

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://todohogarfactory.es',
+  generateRobotsTxt: true, // Esto también te crea el archivo robots.txt automáticamente
+  sitemapSize: 7000,
+}
